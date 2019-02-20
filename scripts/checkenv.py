4fadd639cb0fd8de4812db911f2862e9ba8e9051
penguin <echo leeminsuok@naver.com>
2019-02-20 10:46:05 +0900

    리팩토링 pyside2

--- scripts/checkenv.py
+++ scripts/checkenv.py
@@ -1,11 +1,27 @@
 #coding:utf8
 import os
-import nuke
+from PySide2.QtWidgets import *
+
+class CheckEnv(QWidget):
+	layout = QVBoxLayout()
+	def __init__(self):
+		super(CheckEnv, self).__init__()
+		self.setLayout(self.layout)
+		self.setEnv()
+
+	def setEnv(self):
+		envs = ["USER", "OCIO", "NUKE_PATH", "NUKE_FONT_PATH"]
+		for e in envs:
+			self.layout.addWidget(QLabel("$%s : %s" % (e, os.environ[e])))
 
 def main():
-	results = []
-	envs = ["USER", "OCIO", "NUKE_PATH", "NUKE_FONT_PATH"]
-	for env in envs:
-		results.append("$%s : %s" % (env,os.environ[env]))
-	nuke.message("\n".join(results))
-	
+	global customApp
+	try:
+		customApp.close()
+	except:
+		pass
+	customApp = CheckEnv()
+	try:
+		customApp.show()
+	except:
+		pass
